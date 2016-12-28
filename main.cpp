f392d227b358bc3a9c18d38fbb3da2d701a56019
Pelayo Choya <pelayo.choya@gmail.com>
2016-12-28 19:42:28 +0100

    Final version of the code

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <opencv2/imgproc.hpp>
 #include <string>
 #include <vector>
+#include <fstream>
 #include <iostream>
 
 using namespace std;
@@ -16,11 +17,11 @@ int main(int argc, char *argv[])
     string extension = ".jpg";
     vector < Mat > vector_im, corners_im, gray_im;
     Mat im,gray;
-    Size patternsize(7,9);
-    int board_height = 205;
-    int board_width = 160;
-    float measure = 22.75;
 
+    float measure = 22.5;
+    int board_w=7;
+    int board_h=9;
+    Size patternsize(board_w,board_h);
     vector < Point2f > corners;
     vector< vector< Point2f> > image_points;
     vector< vector< Point3f> > object_points;
@@ -47,14 +48,11 @@ int main(int argc, char *argv[])
        patternfound =  findChessboardCorners(gray_im[k],patternsize,corners,CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE
                                                   + CALIB_CB_FAST_CHECK);
        if(patternfound){
-         //cornerSubPix(vector_im[k], corners, Size(11, 11), Size(-1, -1),
-           //TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
-         //drawChessboardCorners(vector_im[k], patternsize, Mat(corners), patternfound);
            //corner point refine
               cornerSubPix(gray_im[k], corners, Size(11,11), Size(-1,-1), TermCriteria(CV_TERMCRIT_EPS+CV_TERMCRIT_ITER, 30, 0.1));
               //draw corner
               drawChessboardCorners(vector_im[k], patternsize, corners, patternfound);
-              if(corners.size() == board_width*board_height)
+              if(corners.size() == board_w*board_h)
               {
                vector< Point2f> v_tImgPT;
                vector< Point3f> v_tObjPT;
@@ -67,8 +65,8 @@ int main(int argc, char *argv[])
                 tImgPT.x = corners[j].x;
                 tImgPT.y = corners[j].y;
 
-                tObjPT.x = j%board_width*measure;
-                tObjPT.y = j/board_width*measure;
+                tObjPT.x = j%board_w*measure;
+                tObjPT.y = j/board_w*measure;
                 tObjPT.z = 0;
 
                 v_tImgPT.push_back(tImgPT);
@@ -89,10 +87,24 @@ int main(int argc, char *argv[])
       int flag = 0;
       flag |= CV_CALIB_FIX_K4;
       flag |= CV_CALIB_FIX_K5;
-      //calibrateCamera(object_points, image_points, vector_im[0].size(), K, D, rvecs, tvecs, flag);
+      calibrateCamera(object_points, image_points, vector_im[0].size(), K, D, rvecs, tvecs, flag);
 
-    namedWindow("hue",CV_WINDOW_AUTOSIZE);
-    imshow("hue",vector_im[1]);
-    waitKey(0);
+  //Write the necessary parameters to a file
+    ofstream myfile;
+      myfile.open ("parameters.txt");
+      myfile << "Camera Matrix:\n " << K << "\n"<<"Distortion Matrix: \n" << D<< "\n" << "Rotation vectors: \n";
+                for(int u = 0; u < rvecs.size(); u++){
+                    stringstream dd;
+                    dd << (u + 1);
+                    myfile<< "Image"+ dd.str()+ "\t" << rvecs[u] << endl;
+                }
+      myfile << "Translation vectors:" << "\n";
+      for(int f = 0; f < tvecs.size(); f++){
+          stringstream dd;
+          dd << (f + 1);
+          myfile<< "Image"+ dd.str()+ "\t" << rvecs[f] << endl;
+      }
+      myfile.close();
+
     return 0;
 }
